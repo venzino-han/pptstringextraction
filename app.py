4638d156a4c41c07ee2c6917be2f0fac63f07842
virkillthenasty <46100518+virkillthenasty@users.noreply.github.com>
2020-06-10 11:15:34 +0900

    init

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, url_for, request, send_file, after_this_request
+from werkzeug.utils import secure_filename
+import os
+from extraction import extraction
+
+app= Flask(__name__)
+
+
+@app.route('/', methods =['POST','GET'])
+def index():
+   return render_template('index.html')
+
+
+
+@app.route('/upload', methods =['POST','GET'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        filename = secure_filename(f.filename)
+        return downloadFile(str(filename))
+
+
+# @app.route('/download')
+def downloadFile (filename):
+    cwd = os.getcwd()
+    newFileName = extraction(filename)
+    path = os.path.join(cwd, newFileName)
+    return send_file(path, as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run(host="192.168.153.197",debug=True)
---
+++ extraction.py
@@ -0,0 +1,38 @@
+# string extraction
+from pptx import Presentation
+import pandas as pd
+import re
+import os
+
+def extraction(filename):
+    prs = Presentation(filename)
+    result = []
+    i = 0
+    for slide in prs.slides:
+        i += 1
+        if len(slide.shapes) == 0:
+            continue
+        else:
+            title = slide.shapes.title.text
+            for shape in slide.shapes:
+                if shape.has_table:
+                    table = shape.table
+                    for r in table.rows:
+                        for c in r.cells:
+                            txt = ''
+                            for t in c.text_frame.paragraphs:
+                                txt += t.text
+                            strings = re.findall(r'“(.*?)”', txt)
+                            for s in strings:
+                                result.append([i, title, s])
+
+                if hasattr(shape, "text"):
+                    strings = re.findall(r'“(.*?)”', shape.text)
+                    for s in strings:
+                        result.append([i, title, s])
+
+    df = pd.DataFrame(result)
+    newFileName = filename[:-4]+'csv'
+    os.remove(filename)
+    df.to_csv(newFileName)
+    return newFileName
